b3aff964c5f1580c1b441e3be4619bb0eeb686df
Edmund044 <edmundopiyo@gmail.com>
2021-03-08 16:12:51 +0300

    backup.php
    backup

---
+++ backup.php
@@ -0,0 +1,75 @@
+<div class="card" style="width: 100%;">
+  <img src="./images/011nnONQ.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+  </div>
+</div>
+///
+<h2>Programming Training</h2>
+<div class="card" style="width: 100%;">
+  <img src="./images/011nnONQ.jpeg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+  </div>
+</div>
+///
+ 
+<div id="snaptarget" style="width:100%;height:75%;background-color:whitesmoke;">
+  <p>I&apos;m a snap target</p>
+</div>
+ 
+<br style="clear:both">
+ <div class="container">
+    <div class="row">
+      <div class="col-3 w-30 h-30 bg-white" id="draggable">
+rgrgrg
+      </div>
+   <div class="col-3 w-30 h-30 bg-white" id="draggable2">
+fgrr
+      </div>
+ <div class="col-3 w-30 h-30 bg-white" id="draggable3">
+grgr
+      </div>
+ <div class="col-3 w-30 h-30 bg-white" id="draggable4">
+rgrgr
+      </div>
+ 
+
+</div>
+</div>
+/////
+
+<h2>Programming Training</h2>
+<h5>Which of the following are towns/cities in Kenya?</h5>
+<label class="container">Nairobi
+  <input type="checkbox" >
+  <span class="checkmark"></span>
+</label>
+<label class="container">New York
+  <input type="checkbox" >
+  <span class="checkmark"></span>
+</label>
+<label class="container">Kakamega
+  <input type="checkbox" >
+  <span class="checkmark"></span>
+</label>
+<label class="container">Windhoek
+  <input type="checkbox" >
+  <span class="checkmark"></span>
+</label>
+/////
+<form action="multiple.php" method="POST" id="form"> 
+<input type="radio" id="male" name="gender" value="correct">
+  <label for="male">Male</label><br>
+  <input type="radio" id="female" name="gender" value="female" required>
+  <label for="female">Female</label><br>
+ <input type="radio" id="male" name="gender" value="male" required>
+  <label for="male">Male</label><br>
+  <input type="radio" id="female" name="gender" value="female" required>
+  <label for="female">Female</label><br>
+<input type="button" name="submit"  class="btn btn-outline btn-secondary text-light" id="submit" value="Submit Answer">
+</form>
